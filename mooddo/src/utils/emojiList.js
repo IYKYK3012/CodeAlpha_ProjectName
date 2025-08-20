4831c4cb9aed23b342f31ca08579c023cdfdf59e
Geetanjali <vishwakarmageetanjali6@gmail.com>
2025-08-20 22:19:33 +0530

    Removed mooddo and gesture projects from repo

--- mooddo/src/utils/emojiList.js
+++
@@ -1,9 +0,0 @@
-const emojiList = [
-  { emoji: '😊', label: 'happy', colorClass: 'happy' },
-  { emoji: '💪', label: 'productive', colorClass: 'productive' },
-  { emoji: '😌', label: 'calm', colorClass: 'calm' },
-  { emoji: '😖', label: 'stressed', colorClass: 'stressed' },
-  { emoji: '😴', label: 'tired', colorClass: 'tired' },
-];
-
-export default emojiList;
